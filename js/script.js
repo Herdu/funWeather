7c633cfd1a793414606fade5aab0048f51d89a47
Matt Dob <mateusz.dobrowolski71@gmail.com>
2017-02-26 19:06:19 +0100

    slider

--- js/script.js
+++ js/script.js
@@ -1,10 +1,61 @@
-$( ".drag" ).mouseover(function() {
 
-    $(".drag").each(function() {
-        $(this).removeClass("drag-hover");
+$(document).ready(function(){
+
+
+    $("#download-button").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#footer").offset().top
+        }, 2000);
+    });
+
+    $("#download-btn").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#footer").offset().top
+        }, 2000);
+    });
+
+    $("#features-button").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#features").offset().top
+        }, 2000);
+    });
+
+    $("#features-btn").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#features").offset().top
+        }, 2000);
+    });
+
+    $("#home-button").click(function () {
+        $('html, body').animate({
+            scrollTop: $(".header").offset().top
+        }, 2000);
+    });
+
+    $(".navbar-brand").click(function () {
+        $('html, body').animate({
+            scrollTop: $(".header").offset().top
+        }, 2000);
     });
 
 
-    $(this).addClass("drag-hover");
-    console.log("yolo");
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: true,
+        variableWidth: true,
+        width: 317
+    });
+
+
+
+
+    // On before slide change
+    $('.slider').on('afterChange', function(event, slick, currentSlide, nextSlide){
+        $('.slick-center').removeClass( "slick-right" ).removeClass("slick-left");
+        $('.slick-center').prev().removeClass( "slick-right" ).addClass( "slick-left" );
+        $('.slick-center').next().removeClass( "slick-left" ).addClass( "slick-right" );
+    });
+
 });
