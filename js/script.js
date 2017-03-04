cd8566f3bd88cd5050c8e21307e29cc205e45db6
Matt Dob <mateusz.dobrowolski71@gmail.com>
2017-03-04 13:40:47 +0100

    scrolldown button

--- js/script.js
+++ js/script.js
@@ -1,61 +1,59 @@
 
-$(document).ready(function(){
+$(document).ready(function() {
 
 
     $("#download-button").click(function () {
         $('html, body').animate({
             scrollTop: $("#footer").offset().top
-        }, 2000);
+        }, 1000);
     });
 
     $("#download-btn").click(function () {
         $('html, body').animate({
             scrollTop: $("#footer").offset().top
-        }, 2000);
+        }, 1000);
     });
 
     $("#features-button").click(function () {
         $('html, body').animate({
             scrollTop: $("#features").offset().top
-        }, 2000);
+        }, 1000);
     });
+
+    $("#scroll-down").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#features").offset().top
+        }, 1000);
+    });
+
+
+
 
     $("#features-btn").click(function () {
         $('html, body').animate({
             scrollTop: $("#features").offset().top
-        }, 2000);
+        }, 1000);
     });
 
     $("#home-button").click(function () {
         $('html, body').animate({
             scrollTop: $(".header").offset().top
-        }, 2000);
+        }, 1000);
     });
 
     $(".navbar-brand").click(function () {
         $('html, body').animate({
             scrollTop: $(".header").offset().top
-        }, 2000);
+        }, 1000);
     });
 
 
-    $('.slider').slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        centerMode: true,
-        variableWidth: true,
-        width: 317
-    });
-
-
-
-
-    // On before slide change
-    $('.slider').on('afterChange', function(event, slick, currentSlide, nextSlide){
-        $('.slick-center').removeClass( "slick-right" ).removeClass("slick-left");
-        $('.slick-center').prev().removeClass( "slick-right" ).addClass( "slick-left" );
-        $('.slick-center').next().removeClass( "slick-left" ).addClass( "slick-right" );
+    $("#reviews-button").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#reviews").offset().top
+        }, 1000);
     });
 
 });
+
+
